6913a8a714f45d3945d34220d46edefb0ff841a5
DishaGup <guptamansi201@gmail.com>
2022-12-15 15:09:41 +0530

    funciotnality add

---
+++ day-1/day-3/scripts/index.js
@@ -0,0 +1,12 @@
+let login=document.getElementById("login")
+let container=document.getElementById("login-container")
+//login.addEventListener("click",shownumber)
+
+
+
+function shownumber(){
+
+container.style.right="0";
+container.style.opacity="5";
+container.style.display="block"
+}
--- day-1/scripts/index.js
+++ day-1/scripts/index.js
@@ -1,8 +1,22 @@
 
 let login=document.getElementById("login")
-login.addEventListener("click",()=>{
+let container=document.getElementById("login-container")
+//login.addEventListener("click",shownumber)
+let dropmenu=document.getElementById("number-div-drop")
 
-    
 
+function shownumber(){
 
-})
+container.style.right="0";
+container.style.opacity="5";
+container.style.display="block"
+}
+function hidenumber() {
+    container.style.right="-300px";
+container.style.opacity="0";
+container.style.display="block"
+dropmenu.style.opacity="0"
+}
+function showdropmenu() {
+    dropmenu.style.opacity="100%"
+}
