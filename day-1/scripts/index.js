8186a97930036b3f65526e2689f957d98a25aa28
DishaGup <guptamansi201@gmail.com>
2022-12-17 16:07:43 +0530

    added

--- day-1/scripts/index.js
+++ day-1/scripts/index.js
@@ -1,17 +1,23 @@
+
 let login=document.getElementById("login")
 let container=document.getElementById("login-container")
 //login.addEventListener("click",shownumber)
 let dropmenu=document.getElementById("number-div-drop")
 let showdrop=document.getElementById("upper-country")
-let sideDrop=document.getElementById("side-div-drop")
+//let sendAlert=document.getElementById("send-book")
+
+function sendAlert(){
+ alert("OTP sent to registered number")
+}
+
+
+
 
 function shownumber(){
 
 container.style.right="0";
-container.style.opacity="100";
+container.style.opacity="5";
 container.style.display="block"
-
-
 }
 function hidenumber() {
     container.style.right="-300px";
@@ -20,23 +26,19 @@ container.style.display="block"
 dropmenu.style.opacity="0"
 dropmenu.style.top="-400px"
 }
+   
+
 function showdropmenu() {
-    dropmenu.style.opacity="100%"
+    dropmenu.style.opacity="100"
     dropmenu.style.top="200px"
 }
 
 function showcountrydrop() {
     showdrop.style.transform="translateY(0%)"
     showdrop.style.opacity="10"
+    showdrop.style.display="block"
 }
-
 function hidecountrydrop(){
-    showdrop.style.transform="translateY(-120%)"
+    showdrop.style.transform="translateY(-100%)"
     showdrop.style.opacity="0"
-}
-function hideside(){
-sideDrop.style.right="-400px"
-}
-function showside(){
-    sideDrop.style.right="0"
 }
